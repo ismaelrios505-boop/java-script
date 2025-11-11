9d1a973f6006b36e75d46e1ffa824d85197c1ac8
Ismael Rios <ismaelrios505@gmail.com>
2025-11-10 23:13:53 -0500

    Actualizado buscarpalabra sin usar split()

--- guia4.js/11.js
+++ guia4.js/11.js
@@ -1,17 +1,24 @@
 function buscarpalabra() {
     let frase = prompt("Ingrese una frase:");
     let palabra = prompt("Ingrese la palabra a buscar:");
-    frase= frase.split(" ");
+    let palabraTemp = "";
     let encontrado = false;
-    for (let i = 0; i < frase.length; i++) {
-        if (frase[i] === palabra) {
-            encontrado = true;
-            break;
+
+    for (let i = 0; i <= frase.length; i++) {
+        if (frase[i] === " " || i === frase.length) {
+            if (palabraTemp === palabra) {
+                encontrado = true;
+                break;
+            }
+            palabraTemp = ""; 
+        } else {
+            palabraTemp += frase[i]; 
         }
     }
-    if (encontrado===true) {
-        console.log(`La palabra "${palabra}" si existe en el texto.`);
+
+    if (encontrado === true) {
+        console.log(`La palabra "${palabra}" sí existe en el texto.`);
     } else {
         console.log(`La palabra "${palabra}" no existe en el texto.`);
-    }   
+    }
 }
